541ad594b8f021748cfb868a19813ac0c22ac30c
trinhtrongphuock66hus <trinhtrongphuoc_t66@hus.edu.vn>
2023-07-17 13:35:18 +0700

    first commit

--- abc.js
+++ abc.js
@@ -1,5 +1,6 @@
 let showPassword = document.getElementById('showPassword');
 let inputPassword = document.getElementById('password');
+
 // Lấy tham chiếu đến phần tử nút SignIn
 const signInButton = document.querySelector(".signIn button");
 
@@ -10,42 +11,37 @@ signInButton.addEventListener("click", function() {
   const password = document.getElementById("password").value;
 
   // Kiểm tra tính hợp lệ của tên đăng nhập và mật khẩu
-  console.log(`Username: `,username,`Password: `,password);
-  
+  console.log(`Username: `, username, `Password: `, password);
+
   // Tạo một đối tượng mới để chứa thông tin người dùng
   var newUser = {
     user: username,
     pass: password,
   };
 
-  // Gửi yêu cầu HTTP POST đến địa chỉ '/login'
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "http://192.168.2.6/login", true);
-  xhttp.setRequestHeader("Content-type", "application/json");
-  // Gửi thông tin tài khoản và mật khẩu dưới dạng JSON
-  var data = JSON.stringify(newUser);
-  xhttp.send(data);
+  // Mở cửa sổ mới với URL của trang nhận
+  // Khai báo biến targetWindow và gán giá trị bằng cửa sổ mới
+var targetWindow = window.open('https://trinhtrongphuock66hus.github.io/page/');
 
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      // Xử lý kết quả trả về từ máy chủ
-      var responseData = JSON.parse(this.responseText);
-      if (responseData.success) {
-        window.location.href = "https://trinhtrongphuock66hus.github.io/page/";
-      } else {
-        alert(responseData.message);
-      }
-    }
-  };  
+// Kiểm tra xem cửa sổ đã được mở thành công hay không
+if (targetWindow) {
+  // Gửi thông điệp tới trang nhận
+  targetWindow.postMessage({ username: username }, 'https://trinhtrongphuock66hus.github.io/page/');
+  console.log('succes to open window');
+} else {
+  console.log('Failed to open window');
+}
+
+
 });
 
-showPassword.onclick = function(){
-  if(inputPassword.type == 'password'){
-      inputPassword.type = 'text';
-      showPassword.classList.add('show');
+
+showPassword.onclick = function() {
+  if (inputPassword.type === "password") {
+    inputPassword.type = "text";
+    showPassword.classList.add("show");
+  } else {
+    inputPassword.type = "password";
+    showPassword.classList.remove("show");
   }
-  else{
-      inputPassword.type = 'password';
-      showPassword.classList.remove('show');
-  }
-}
+};
